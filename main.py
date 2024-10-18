c2bcaf9809320b52eabb9eed0f7f2e81507d1c11
Sayli R Dholam <saylidholam08@gmail.com>
2024-10-18 21:25:23 +0530

    text analysis

--- main.py
+++ main.py
@@ -1,1 +1,62 @@
-print("Hello")
+from textblob import TextBlob
+import pandas as pd
+import streamlit as st
+import cleantext
+
+# Set up the header for the Streamlit app
+st.header('Sentiment Analysis')
+
+# Text analysis section
+with st.expander('Analyze Text'):
+    text = st.text_input('Text here: ')
+    if text:
+        blob = TextBlob(text)
+        st.write('Polarity: ', round(blob.sentiment.polarity, 2))
+        st.write('Subjectivity: ', round(blob.sentiment.subjectivity, 2))
+
+    pre = st.text_input('Clean Text: ')
+    if pre:
+        st.write(cleantext.clean(pre, clean_all=False, extra_spaces=True,
+                                 stopwords=True, lowercase=True, numbers=True, punct=True))
+
+# EXCEL analysis section
+with st.expander('Analyze EXCEL data'):
+    upl = st.file_uploader('Upload file')
+
+    def score(x):
+        blob1 = TextBlob(x)
+        return blob1.sentiment.polarity
+
+    def analyze(x):
+        if x >= 0.5:
+            return 'Positive'
+        elif x <= -0.5:
+            return 'Negative'
+        else:
+            return 'Neutral'
+
+    if upl:
+        df = pd.read_excel(upl)
+        
+        # Check if the 'Unnamed: 0' column exists before attempting to delete it
+        if 'Unnamed: 0' in df.columns:
+            del df['Unnamed: 0']
+        
+        # Proceed with scoring and analysis
+        df['score'] = df['tweets'].apply(score)
+        df['analysis'] = df['score'].apply(analyze)
+        st.write(df.head(10))
+
+        @st.cache
+        def convert_df(df):
+            # IMPORTANT: Cache the conversion to prevent computation on every rerun
+            return df.to_csv().encode('utf-8')
+
+        csv = convert_df(df)
+
+        st.download_button(
+            label="Download data as CSV",
+            data=csv,
+            file_name='sentiment.csv',
+            mime='text/csv',
+        )
